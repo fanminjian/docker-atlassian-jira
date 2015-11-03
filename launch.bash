055eb7bf7df5eac65fe7e3513364cd0377a4cfab
Jimmy Fan <fanminjian@gmail.com>
2015-11-03 23:38:46 +1100

    Remove database part

--- launch.bash
+++ launch.bash
@@ -32,35 +32,4 @@ if [ -n "$CONNECTOR_SCHEME" ]; then
 	xmlstarlet ed --inplace --insert "/Server/Service/Connector" --type attr -n scheme -v $CONNECTOR_SCHEME conf/server.xml
 fi
 
-if [ -n "$DATABASE_URL" ]; then
-  extract_database_url "$DATABASE_URL" DB /opt/jira/lib
-  DB_JDBC_URL="$(xmlstarlet esc "$DB_JDBC_URL")"
-  SCHEMA=''
-  if [ "$DB_TYPE" != "mysql" ]; then
-    SCHEMA='<schema-name>public</schema-name>'
-  fi
-
-  cat <<END > /opt/atlassian-home/dbconfig.xml
-<?xml version="1.0" encoding="UTF-8"?>
-<jira-database-config>
-  <name>defaultDS</name>
-  <delegator-name>default</delegator-name>
-  <database-type>$DB_TYPE</database-type>
-  $SCHEMA
-  <jdbc-datasource>
-    <url>$DB_JDBC_URL</url>
-    <driver-class>$DB_JDBC_DRIVER</driver-class>
-    <username>$DB_USER</username>
-    <password>$DB_PASSWORD</password>
-    <pool-min-size>20</pool-min-size>
-    <pool-max-size>20</pool-max-size>
-    <pool-max-wait>30000</pool-max-wait>
-    <pool-max-idle>20</pool-max-idle>
-    <pool-remove-abandoned>true</pool-remove-abandoned>
-    <pool-remove-abandoned-timeout>300</pool-remove-abandoned-timeout>
-  </jdbc-datasource>
-</jira-database-config>
-END
-fi
-
 /opt/atlassian/jira/bin/start-jira.sh -fg
